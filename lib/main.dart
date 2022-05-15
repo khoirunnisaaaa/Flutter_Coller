1ff20a7c93816e7b9390d1de9465faed2a0abe68
pujirahayu01 <puji89503@gmail.com>
2022-05-16 00:58:30 +0700

    register

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:coller_mobile/view/Dashboard.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());

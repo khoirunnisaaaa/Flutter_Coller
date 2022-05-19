9e3dedd25d14411f66962e849bf1f7870726a132
Khoirunnisa <k.nisa11602@gmail.com>
2022-05-19 19:52:41 +0700

    Update theme.dart

--- lib/theme.dart
+++ lib/theme.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 Color strongRedColor = Color(0xffFB4F4F);
 Color redColor = Color(0xffF76963);
+Color bgLightRedColor = Color(0xffFDF1EF);
 Color lightGreyColor = Color(0xffF3F3F3);
 Color greyColor = Color(0xffEDEDED);
 Color darkGreyColor = Color(0xff464646);
@@ -15,3 +16,8 @@ TextStyle mainTitleTextStyle =
     TextStyle(color: darkGreyColor, fontWeight: FontWeight.w600, fontSize: 18);
 TextStyle subMenuTextStyle = TextStyle(
     color: semiDarkGreyColor, fontWeight: FontWeight.w600, fontSize: 13);
+TextStyle titleTFEditProfile = TextStyle(
+  fontSize: 14,
+  fontWeight: FontWeight.w500,
+  color: Color(0xff949494),
+);
